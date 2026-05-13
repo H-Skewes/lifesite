9b714e5ed1c31ad00636cbdd873791bf4a580b7e
Henry Skewes <info@henryskewes.com>
2026-05-13 16:41:41 -0500

    filtered weather data for api

--- src/backend/services/weather.py
+++ src/backend/services/weather.py
@@ -9,8 +9,13 @@ url = os.getenv("WEATHERURL")
 def weather_request():
     response = requests.get(url)
     if response.status_code == 200:
-        weatherdata = response.json()
+        weatherdata = filter_weather_data(response.json())
         return weatherdata
     else:
         return response.status_code
-    
+
+
+def filter_weather_data(response):
+    filtered_location = response["location"]["name"] + response["location"]["region"]
+    filtered_data = f'{{"city": {response["location"]["name"]}, "state": {response["location"]["region"]}, "feelslike": {response["current"]["feelslike_f"]}, "windspeed": {response["current"]["wind_mph"]}, "rainpossibility": {response["current"]["chance_of_rain"]}, "snowpossibility": {response["current"]["chance_of_snow"]}, "uv": {response["current"]["uv"]}}}'
+    return filtered_data
